e238c3a5bec155036c580c91c5b0dc5400dcf07c
wucheng <wucheng@staff.sina.com.cn>
2017-04-15 10:01:47 +0800

    delete useless import

--- tests/runtests.py
+++ tests/runtests.py
@@ -3,7 +3,6 @@
 # vim: fileencoding=utf-8 autoindent tabstop=4 shiftwidth=4 expandtab softtabstop=4 filetype=python
 
 import unittest
-from tests.test_transition import TestTransition
 
 def main():
     discover = unittest.defaultTestLoader.discover('./tests', pattern='test*.py')
